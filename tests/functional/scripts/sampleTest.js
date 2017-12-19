8f4c4a9f9e9d59f6dcb0b3a4a367af28b8b58829
Luiz Gustavo Rodrigues Gonzalez <lois@br.ibm.com>
2017-12-19 11:14:40 -0200

    Added Test Case

--- tests/functional/scripts/sampleTest.js
+++ tests/functional/scripts/sampleTest.js
@@ -1,12 +1,14 @@
 module.exports = {
   'Sample functional test' : function (browser) {
     browser
-      .url('http://google.com')
-      .waitForElementVisible('input.gsfi', 60000, true,
-        function(){}, 'Waiting for Search Input to become visible')
-      .setValue('input.gsfi', ['functional tests', browser.Keys.ENTER])
-      .waitForElementPresent('#searchform', 60000)
+      .url('http://cvadvisor-test.mybluemix.net')
+      .waitForElementVisible('#button1', 60000, true,
+        function(){}, 'Waiting for Cadastre-se Agora button to become visible')
+      .click('#button1')
+      .waitForElementVisible('#button2', 60000, true,
+        function(){}, 'Waiting for Cadastre-se aqui button to become visible')
       .pause(5000)
+      .click('#button2')
       .end();
   }
 };
